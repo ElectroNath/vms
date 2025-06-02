b44b6b685733263343619f9733edbf5aa6579d7f
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-06-02 11:47:33 +0100

    fix: update ngrok URL references in settings and API configuration

--- fe/VMS-FRONT-END/src/api.js
+++ fe/VMS-FRONT-END/src/api.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://6854-102-88-111-46.ngrok-free.app";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://a3a5-102-88-111-89.ngrok-free.app";
